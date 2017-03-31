7cb705cca0e144f9008730117e60bb5e3f842ca0
Andr3aCbb <Andr3aCbb@users.noreply.github.com>
2017-03-31 15:02:02 +0200

    Add files via upload

---
+++ Data-preprocessing.R
@@ -0,0 +1,119 @@
+
+#Read .csv of preprocessed clinical data and assign to 'diabetic data'
+diabetic_data<- read.csv(file="/home/acabbia/datasets/Diabetes_130_US_hospitals_99_08_dataset/OGO-compbio.csv",head=TRUE,sep=",")
+
+
+#####turn diagnosis 1 into 'Musculoskeletal [disease]' or 'Other'###################################################
+##### TO DO: turn diagnosis 1 code into name ######################################################
+
+diabetic_data$diag_1=as.numeric(diabetic_data$diag_1)
+
+##### Delete some features and encounters#########################################################################
+library(plyr)
+library(dplyr)
+
+#delete features 'encounter_id', 'patient_id', 'payer code' ,'medical specialty', 'glucose serum', 'weight'
+# ( missing data > 50% )
+diabetic_data=dplyr::select(diabetic_data,-encounter_id)
+diabetic_data=dplyr::select(diabetic_data,-patient_nbr)
+diabetic_data=dplyr::select(diabetic_data,-payer_code) 
+diabetic_data=dplyr::select(diabetic_data,-medical_specialty)
+diabetic_data=dplyr::select(diabetic_data, -max_glu_serum)
+diabetic_data=dplyr::select(diabetic_data,-weight)
+#remove diagnoses 2 and 3
+diabetic_data=dplyr::select(diabetic_data,-diag_2)
+diabetic_data=dplyr::select(diabetic_data,-diag_3)
+#remove all medications
+meds=c('examide','citoglipton','nateglinide','glimepiride.pioglitazone','chlorpropamide','acetohexamide','tolbutamide','pioglitazone','acarbose','miglitol',
+                      'troglitazone','tolazamide','glyburide.metformin','glipizide.metformin','metformin.rosiglitazone','metformin.pioglitazone','metformin', 'repaglinide','glimepiride',
+                      'glipizide','glyburide','rosiglitazone') 
+for (feature in meds)
+{diabetic_data=dplyr::select(diabetic_data,-eval(parse(text = feature)))}
+
+### drop patients with gender = unknown/invalid 
+diabetic_data$gender<-plyr::revalue(diabetic_data$gender, c("Unknown/Invalid"="Male"))
+
+#delete patient encounters with discharge_disposition_id 11, 13, 14, 19, 20 or 21
+for (out in c(11, 13, 14, 19, 20, 21))
+{diabetic_data=diabetic_data[diabetic_data$discharge_disposition_id != out ,]}
+
+
+##################################################################################################################
+
+######## Make 'id' features into their corresponding qualitative values  ######## 
+
+#turn admission_type_id into corresponding qualitive values
+admission_types=c('Emergency','Urgent','Elective','Newborn','Not Available','NULL','Trauma Center','Not Mapped')
+for (index in 1:length(diabetic_data$admission_type_id))
+{i=1
+for (admissiontype in admission_types)
+{if (diabetic_data$admission_type_id[index]==i)
+{diabetic_data$admission_type_id[index]=admissiontype
+break}
+  else {i=i+1}}}
+diabetic_data$admission_type_id=factor(diabetic_data$admission_type_id)
+
+#turn discharge_disposition_id into corresponding qualitive values if value is among the 6 most frequently occurring IDs, 
+#else assigned 'other'
+
+for (index in 1:length(diabetic_data$discharge_disposition_id))
+{if (diabetic_data$discharge_disposition_id[index]==1)
+{diabetic_data$discharge_disposition_id[index]='Discharged to home'}
+  else if (diabetic_data$discharge_disposition_id[index]==2)
+  {diabetic_data$discharge_disposition_id[index]='Discharged/transferred to another short term hospital'}
+  else if (diabetic_data$discharge_disposition_id[index]==3)
+  {diabetic_data$discharge_disposition_id[index]='Discharged/transferred to SNF'}
+  else if (diabetic_data$discharge_disposition_id[index]==6)
+  {diabetic_data$discharge_disposition_id[index]='Discharged/transferred to home with home health service'}
+  else if (diabetic_data$discharge_disposition_id[index]==18)
+  {diabetic_data$discharge_disposition_id[index]='NULL'}
+  else if (diabetic_data$discharge_disposition_id[index]==22)
+  {diabetic_data$discharge_disposition_id[index]='Discharged/transferred to another rehab fac including rehab units of a hospital .'}
+  else {diabetic_data$discharge_disposition_id[index]='Other'}}
+
+diabetic_data$discharge_disposition_id=factor(diabetic_data$discharge_disposition_id)
+
+#turn admission_source_id into corresponding qualitive values if value is among the 6 most frequently occurring IDs, 
+#else assigned 'other'
+for (index in 1:length(diabetic_data$admission_source_id))
+{if (diabetic_data$admission_source_id[index]==1)
+{diabetic_data$admission_source_id[index]='Physician Referral'}
+  else if (diabetic_data$admission_source_id[index]==2)
+  {diabetic_data$admission_source_id[index]='Clinic Referral'}
+  else if (diabetic_data$admission_source_id[index]==4)
+  {diabetic_data$admission_source_id[index]='Transfer from a hospital'}
+  else if (diabetic_data$admission_source_id[index]==6)
+  {diabetic_data$admission_source_id[index]='Transfer from another health care facility'}
+  else if (diabetic_data$admission_source_id[index]==7)
+  {diabetic_data$admission_source_id[index]='Emergency Room'}
+  else if (diabetic_data$admission_source_id[index]==18)
+  {diabetic_data$admission_source_id[index]='Transfer From Another Home Health Agency'}
+  else {diabetic_data$admission_source_id[index]='Other'}}
+
+diabetic_data$admission_source_id=factor(diabetic_data$admission_source_id)
+
+#############################################################################
+### change readmitted  (3 levels) from "<30", ">30" , "NO" to "YES", "NO" (2 levels)
+############################################################################
+
+diabetic_data$readmitted<-plyr::revalue(diabetic_data$readmitted, c("<30"="YES", ">30"="YES"))
+
+
+#############################################################################
+#### subset of patients who have the A1c measure 
+#############################################################################
+
+A1c <- filter(diabetic_data, A1Cresult != "None")
+###############################################################
+
+# Clean up #
+rm(admission_types, admissiontype, feature, i, index, meds, out)
+
+###call feature selection script
+source('~/Desktop/feature-selection.R')
+
+
+# Script output: 
+#A1c : preprocessed df of all patients which have a HbA1c measure
+#diabetic_data : preprocessed df 
+
---
+++ feature_selection_statistical_learning.R
@@ -0,0 +1,134 @@
+#####perform machine learning algoritme for feature selection#############################
+#load the libraries
+library(DMwR)
+library(plyr)
+library(dplyr)
+library(caret)
+library(ranger)
+library(doParallel)	
+
+#########################################################
+#### select here the data to analyze: A1C (only subjects with HbA1c measure) or diabetic_data (full dataset)
+
+d_data<-diabetic_data
+# fix class imbalance resampling from less represented class
+
+barplot(table(d_data$readmitted))
+d_data<-DMwR::SMOTE(readmitted~.,data = d_data)
+barplot(table(d_data$readmitted))
+
+#determine predictors (x) and outcome variable (y)
+#create train and test set (70/30)
+
+x=select(d_data, -readmitted)
+y=factor(d_data$readmitted)
+
+idx=sort(sample(nrow(d_data),nrow(d_data)*.7)) 
+train.X=x[idx,]
+train.Y=y[idx]
+test.X=x[-idx,] 
+test.Y=y[-idx]
+
+# ensure the results are repeatable
+set.seed(666)
+
+# define the control options
+ctrl <- trainControl(method = "cv",   
+                     number = 10,   # 10fold cross validation
+                     allowParallel = TRUE)
+
+# Set up parallel processing   
+registerDoParallel()		# Registrer a parallel backend for train
+getDoParWorkers()
+
+# run the ranger algorithm (RF with multicore support) 
+# to find variable importance, peform recursive feature elimination for all the variables (x) on the feature 'readmitted'(y) USING ALL THE DATASET
+resultsRF <- train(x,y, method = "ranger", trControl=ctrl ,importance="permutation",metric="Kappa")
+# summarize the results
+resultsRF
+# list the importance of features
+varImp(resultsRF)
+
+##### rf and logit classification models training #############################
+#model.RF <- train(train.X,train.Y, method = "ranger", trControl=ctrl,metric="Kappa")
+model.LR <- train(train.X,train.Y, method = 'glm', family = binomial, metric="Kappa")
+
+mx.set.seed(666)
+model.MX <- mx.mlp(train.X, train.Y, hidden_node=10, out_node=2, out_activation="softmax",
+                num.round=20, array.batch.size=15, learning.rate=0.07, momentum=0.9,
+                eval.metric=mx.metric.accuracy)
+
+##### Perform prediction for test data ##################
+
+fitted.results.RF = predict(model.RF,newdata=test.X)
+fitted.results.LR = predict(model.LR,newdata=test.X)
+
+#### Evaluate results RF classification
+##Total Accuracy
+
+cm.rf=as.matrix(table(test.Y, fitted.results.RF))
+
+AccuracyRF=sum(diag(cm.rf)/length(test.Y))
+print(paste('Overall accuracy Random Forest classification: ',AccuracyRF))
+##Precision: fraction of correct predictions for a class
+PrecisionRF=diag(cm.rf)/colSums(cm.rf)
+print(paste('Precision class 1: Correct YES predictions',PrecisionRF[1]))
+print(paste('Precision class 2: Correct NO predictions',PrecisionRF[2]))
+##Recall: fraction of instances of a class that were correctly predicted
+RecallRF=diag(cm.rf)/rowSums(cm.rf)
+print(paste('Recall class 1: fraction of readmitted patients correctly predicted',RecallRF[1]))
+print(paste('Recall class 2: fraction of NON readmitted patients correctly predicted',RecallRF[2]))
+
+cm.lr=as.matrix(table(test.Y, fitted.results.LR))
+
+AccuracyLR=sum(diag(cm.lr)/length(test.Y))
+print(paste('Overall accuracy Logistic Regression classification: ',AccuracyLR))
+##Precision: fraction of correct predictions for a class
+PrecisionLR=diag(cm.lr)/colSums(cm.lr)
+print(paste('Precision class 1: Correct YES prediction', PrecisionLR[1]))
+print(paste('Precision class 2: Correct NO prediction', PrecisionLR[2]))
+##Recall: fraction of instances of a class that were correctly predicted
+RecallLR=diag(cm.lr)/rowSums(cm.lr)
+print(paste('Recall class 1: fraction of readmitted patients correctly predicted',RecallLR[1]))
+print(paste('Recall class 2: fraction of NON readmitted patients correctly predicted',RecallLR[2]))
+
+
+#######################################################################################################################
+#####Redo prediction with only significant top 5 features from random forest feature importance #######################
+SignifVars<-as.data.frame(sort(resultsRF$finalModel$variable.importance, decreasing = TRUE))
+diabetic_data_signif=diabetic_data[,c(as.factor(SignifVars[1:6,]))]  # <--- change the "5" in this line to change the number of significant vars considered 
+
+#################################################################################################
+x=diabetic_data_signif
+y=diabetic_data$readmitted
+
+idx=sort(sample(nrow(x),nrow(x)*.7)) 
+
+train.X.sgf=x[idx,]
+train.Y.sgf=y[idx]
+test.X.sgf=x[-idx,] 
+test.Y.sgf=y[-idx]
+
+modelLR.sgf <- train(train.X.sgf,train.Y.sgf, method = 'glm', family = binomial, metric="Kappa")
+summary(modelLR.sgf)
+
+##### Perform prediction for test top5 variables from RF model ##################
+
+fitted.results.LR.sgf = predict(modelLR.sgf,newdata=test.X.sgf)
+
+#### Evaluate results RF classification
+##Total Accuracy
+
+cm.lr.sgf=as.matrix(table(test.Y.sgf, fitted.results.LR.sgf))
+
+AccuracyLR.sgf=sum(diag(cm.lr.sgf)/length(test.Y.sgf))
+print(paste('Overall accuracy Logistic Regression with top 5 features classification: ',AccuracyLR.sgf))
+##Precision: fraction of correct predictions for a class
+PrecisionLR.sgf=diag(cm.lr.sgf)/colSums(cm.lr.sgf)
+print(paste('Precision class 1: Correct YES predictions',PrecisionLR.sgf[1]))
+print(paste('Precision class 2: Correct NO predictions',PrecisionLR.sgf[2]))
+##Recall: fraction of instances of a class that were correctly predicted
+RecallLR.sgf=diag(cm.lr.sgf)/rowSums(cm.lr.sgf)
+print(paste('Recall class 1: fraction of readmitted patients correctly predicted',RecallLR.sgf[1]))
+print(paste('Recall class 2: fraction of NON readmitted patients correctly predicted',RecallLR.sgf[2]))
+
